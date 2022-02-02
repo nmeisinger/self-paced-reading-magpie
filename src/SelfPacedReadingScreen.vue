d34ae96674bd58d5f5d9147ca3a1dbe4a1fd5e65
nmeisinger <nino.meisinger@student.uni-tuebingen.de>
2022-02-02 15:03:37 +0100

    initial

---
+++ src/SelfPacedReadingScreen.vue
@@ -0,0 +1,29 @@
+<template>
+  <Screen>
+    <Slide>
+      <SelfPacedReading
+        :chunks="sentence"
+        word-pos="next"
+        underline="sentence"
+        trigger="~"
+        :response-times.sync="$magpie.measurements.times"
+        @end="$magpie.saveAndNextScreen()"
+      />
+      <button @click="$magpie.saveAndNextScreen()">Next slide</button>
+    </Slide>
+  </Screen>
+</template>
+
+<script>
+export default {
+  name: 'SelfPacedReadingScreen',
+  props: {
+    sentence: {
+      type: Array,
+      required: true
+    }
+  }
+};
+</script>
+
+<style scoped></style>
