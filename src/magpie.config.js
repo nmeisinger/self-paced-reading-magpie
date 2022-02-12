539ed16b331446c3293cf146dcc8e7c9d9179d79
nmeisinger <nino.meisinger@student.uni-tuebingen.de>
2022-02-12 18:57:18 +0100

    fix path

--- src/magpie.config.js
+++ src/magpie.config.js
@@ -1,10 +1,9 @@
 export default {
-  experimentId: '171',
+  experimentId: '293',
   serverUrl: 'https://magpie-demo.herokuapp.com/',
-  socketUrl: 'wss://magpie-demo.herokuapp.com/socket',
   // this will be used in prolific mode
   completionUrl: 'https://...',
   contactEmail: 'test@random.com',
   // Either 'debug', 'directLink' or 'prolific'
-  mode: 'debug'
+  mode: 'directLink'
 };
